171b9427ff2eb32e3f4813e7a586844ab4bf6bf9
naierii <reianviloria@gmail.com>
2024-07-04 14:08:47 +0800

    test: quick pinch zoom package

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import { useCallback, useRef } from 'react';
+
+
 function App() {
+  const imgRef = useRef(null);
+  const onUpdate = useCallback(({ x, y, scale }) => {
+    const { current: img } = imgRef;
+
+    if (img) {
+      const value = make3dTransformValue({ x, y, scale });
+      console.log('value', value)
+
+      img.style.setProperty("transform", value);
+    }
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +29,9 @@ function App() {
             <img src={logo} className="App-logo" alt="logo" />
           </TransformComponent>
         </TransformWrapper>
+        <QuickPinchZoom onUpdate={onUpdate}>
+          <img ref={imgRef} src={logo} className="App-logo" alt="logo" />
+        </QuickPinchZoom>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
